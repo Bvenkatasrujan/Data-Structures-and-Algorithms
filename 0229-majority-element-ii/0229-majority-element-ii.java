7555f5edbdbea8a23f88943a54dda6c0a64401f7
Venkata Srujan Bellamkonda <venkatasrujanb@gmail.com>
2026-09-18 23:13:53 +0530

    Time: 7 ms (53%), Space: 58.4 MB (5.35%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        int n = nums.length;
+        int threshold = n / 3;
+        List<Integer> result = new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        int i = 0;
+        while (i < n) {
+            int current = nums[i];
+            int count = 0;
+            // Count consecutive occurrences of current element
+            while (i < n && nums[i] == current) {
+                count++;
+                i++;
+            }
+            if (count > threshold) {
+                result.add(current);
+            }
+        }
+
+        return result;
+    }
+}
